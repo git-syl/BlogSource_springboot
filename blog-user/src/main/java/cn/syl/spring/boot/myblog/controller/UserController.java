ac91e329c43082ae3298052c3ca6276f4797e85a
mmdsyl <510806100@qq.com>
2017-09-20 20:14:45 +0800

    add Background management

---
+++ blog-user/src/main/java/cn/syl/spring/boot/myblog/controller/UserController.java
@@ -0,0 +1,112 @@
+package cn.syl.spring.boot.myblog.controller;
+
+import cn.syl.spring.boot.myblog.domain.User;
+import cn.syl.spring.boot.myblog.repository.UserRepository;
+import cn.syl.spring.boot.myblog.service.UserService;
+import cn.syl.spring.boot.myblog.utils.ConstraintViolationExceptionHandler;
+import cn.syl.spring.boot.myblog.vo.Response;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+
+//User控制器
+@RestController //=@Controller 和@ResponseBody的组合
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 查询所有用户
+     * @param async
+     * @param pageIndex
+     * @param pageSize
+     * @param name
+     * @param model
+     * @return
+     */
+    @GetMapping
+    public ModelAndView list(@RequestParam(value="async",required=false) boolean async,
+                             @RequestParam(value="pageIndex",required=false,defaultValue="0") int pageIndex,
+                             @RequestParam(value="pageSize",required=false,defaultValue="10") int pageSize,
+                             @RequestParam(value="name",required=false,defaultValue="") String name,
+                             Model model) {
+
+        Pageable pageable = new PageRequest(pageIndex, pageSize);
+        Page<User> page = userService.listUsersByNameLike(name, pageable);
+        List<User> list = page.getContent();    // 当前所在页面数据列表
+
+        model.addAttribute("page", page);
+        model.addAttribute("userList", list);
+        return new ModelAndView(async==true?"users/list :: #mainContainerRepleace":"users/list", "userModel", model);
+    }
+
+
+    /**
+     * 获取创建表单页面
+     * @param model
+     * @return
+     */
+    @GetMapping("/add")
+    public ModelAndView createForm(Model model) {
+        model.addAttribute("user", new User(null, null, null, null));
+        return new ModelAndView("users/add", "userModel", model);
+    }
+
+    /**
+     * 保存或者修改用户
+     * @param user
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<Response> saveOrUpateUser(User user) {
+
+        try {
+            userService.saveOrUpateUser(user);
+        }  catch (ConstraintViolationException e)  {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "处理成功", user));
+    }
+
+
+    /**
+     * 删除用户
+     * @param id
+     * @param model
+     * @return
+     */
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Response> delete(@PathVariable("id") Long id, Model model) {
+        try {
+            userService.removeUser(id);
+        } catch (Exception e) {
+            return  ResponseEntity.ok().body( new Response(false, e.getMessage()));
+        }
+        return  ResponseEntity.ok().body( new Response(true, "处理成功"));
+    }
+
+    /**
+     * 获取修改用户的界面
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "edit/{id}")
+    public ModelAndView modifyForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return new ModelAndView("users/edit", "userModel", model);
+    }
+}
---
+++ blog-user/src/main/java/cn/syl/spring/boot/myblog/vo/Response.java
@@ -0,0 +1,50 @@
+package cn.syl.spring.boot.myblog.vo;
+
+//响应信息
+public class Response {
+    private boolean success;
+    private String message;
+    private Object body;
+
+    public Response() {
+    }
+
+
+    public Response(boolean success) {
+        this.success = success;
+    }
+
+    public Response(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public Response(boolean success, String message, Object body) {
+        this.success = success;
+        this.message = message;
+        this.body = body;
+    }
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+}
